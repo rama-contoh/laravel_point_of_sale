55261deb7715a3e6ab1beda574aaa372dc1bff7e
rama-contoh <145236174+rama-contoh@users.noreply.github.com>
2024-01-07 11:06:27 +0700

    Dedesani melakukan Update create.blade.php

--- resources/views/suppliers/create.blade.php
+++ resources/views/suppliers/create.blade.php
@@ -1,74 +1,151 @@
 @extends('dashboard.body.main')
 
-@section('specificpagestyles')
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://unpkg.com/gijgo@1.9.14/js/gijgo.min.js" type="text/javascript"></script>
-    <link href="https://unpkg.com/gijgo@1.9.14/css/gijgo.min.css" rel="stylesheet" type="text/css" />
-@endsection
-
 @section('container')
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-12">
-
-            @if (session()->has('success'))
-                <div class="alert text-white bg-success" role="alert">
-                    <div class="iq-alert-text">{{ session('success') }}</div>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <i class="ri-close-line"></i>
-                    </button>
-                </div>
-            @endif
-            @if (session()->has('warning'))
-                <div class="alert text-white bg-warning" role="alert">
-                    <div class="iq-alert-text">{{ session('warning') }}</div>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <i class="ri-close-line"></i>
-                    </button>
-                </div>
-            @endif
-
             <div class="card">
                 <div class="card-header d-flex justify-content-between">
                     <div class="header-title">
-                        <h4 class="card-title">Create Advance Salary</h4>
+                        <h4 class="card-title">Add Supplier</h4>
                     </div>
                 </div>
 
                 <div class="card-body">
-                    <form action="{{ route('advance-salary.store') }}" method="POST">
+                    <form action="{{ route('suppliers.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
+                        <!-- begin: Input Image -->
+                        <div class="form-group row align-items-center">
+                            <div class="col-md-12">
+                                <div class="profile-img-edit">
+                                    <div class="crm-profile-img-edit">
+                                        <img class="crm-profile-pic rounded-circle avatar-100" id="image-preview" src="{{ asset('assets/images/user/1.png') }}" alt="profile-pic">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="input-group mb-4 col-lg-6">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input @error('photo') is-invalid @enderror" id="image" name="photo" accept="image/*" onchange="previewImage();">
+                                    <label class="custom-file-label" for="photo">Choose file</label>
+                                </div>
+                                @error('photo')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                        </div>
+                        <!-- end: Input Image -->
                         <!-- begin: Input Data -->
                         <div class=" row align-items-center">
-                            <div class="form-group col-md-12">
-                                <label for="employee_id">Employee Name <span class="text-danger">*</span></label>
-                                <select class="form-control mb-3" id="employee_id" name="employee_id" required>
-                                    <option selected="" disabled>-- Select Employee --</option>
-                                    @foreach ($employees as $employee)
-                                        <option value="{{ $employee->id }}" {{ old('employee_id') == $employee->id ? 'selected' : '' }}>
-                                            {{ $employee->name }}
-                                        </option>
-                                    @endforeach
+                            <div class="form-group col-md-6">
+                                <label for="name">Supplier Name <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" required>
+                                @error('name')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="shopname">Shop Name <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control @error('shopname') is-invalid @enderror" id="shopname" name="shopname" value="{{ old('shopname') }}" required>
+                                @error('shopname')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="email">Supplier Email <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required>
+                                @error('email')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="phone">Supplier Phone <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control @error('phone') is-invalid @enderror" id="phone" name="phone" value="{{ old('phone') }}" required>
+                                @error('phone')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="account_holder">Account Holder</label>
+                                <input type="text" class="form-control @error('account_holder') is-invalid @enderror" id="account_holder" name="account_holder" value="{{ old('account_holder') }}">
+                                @error('account_holder')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="bank_name">Bank Name</label>
+                                <select class="form-control @error('bank_name') is-invalid @enderror" name="bank_name">
+                                    <option value="">Select Year..</option>
+                                    <option value="BRI">BRI</option>
+                                    <option value="BNI">BNI</option>
+                                    <option value="BCA">BCA</option>
+                                    <option value="BSI">BSI</option>
+                                    <option value="Mandiri">Mandiri</option>
                                 </select>
-                                @error('employee_id')
+                                @error('bank_name')
                                 <div class="invalid-feedback">
                                     {{ $message }}
                                 </div>
                                 @enderror
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="datepicker">Date <span class="text-danger">*</span></label>
-                                <input id="datepicker" class="form-control @error('date') is-invalid @enderror" name="date" value="{{ old('date') }}" />
-                                @error('date')
+                                <label for="account_number">Account Number</label>
+                                <input type="text" class="form-control @error('account_number') is-invalid @enderror" id="account_number" name="account_number" value="{{ old('account_number') }}">
+                                @error('account_number')
                                 <div class="invalid-feedback">
                                     {{ $message }}
                                 </div>
                                 @enderror
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="advance_salary">Advance Salary</label>
-                                <input type="text" class="form-control @error('advance_salary') is-invalid @enderror" id="advance_salary" name="advance_salary" value="{{ old('advance_salary') }}">
-                                @error('advance_salary')
+                                <label for="bank_branch">Bank Branch</label>
+                                <input type="text" class="form-control @error('bank_branch') is-invalid @enderror" id="bank_branch" name="bank_branch" value="{{ old('bank_branch') }}">
+                                @error('bank_branch')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="city">Supplier City <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control @error('city') is-invalid @enderror" id="city" name="city" value="{{ old('city') }}" required>
+                                @error('city')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="type">Type of Supplier <span class="text-danger">*</span></label>
+                                <select class="form-control @error('type') is-invalid @enderror" name="type" required>
+                                    <option value="">Select Type..</option>
+                                    <option value="Distributor">Distributor</option>
+                                    <option value="Whole Seller">Whole Seller</option>
+                                </select>
+                                @error('type')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="address">Supplier Address <span class="text-danger">*</span></label>
+                                <textarea class="form-control @error('address') is-invalid @enderror" name="address" required>{{ old('address') }}</textarea>
+                                @error('address')
                                 <div class="invalid-feedback">
                                     {{ $message }}
                                 </div>
@@ -78,7 +155,7 @@
                         <!-- end: Input Data -->
                         <div class="mt-2">
                             <button type="submit" class="btn btn-primary mr-2">Save</button>
-                            <a class="btn bg-danger" href="{{ route('advance-salary.index') }}">Cancel</a>
+                            <a class="btn bg-danger" href="{{ route('suppliers.index') }}">Cancel</a>
                         </div>
                     </form>
                 </div>
@@ -88,11 +165,5 @@
     <!-- Page end  -->
 </div>
 
-<script>
-    $('#datepicker').datepicker({
-        uiLibrary: 'bootstrap4',
-        format: 'yyyy-mm-dd'
-        // https://gijgo.com/datetimepicker/configuration/format
-    });
-</script>
+@include('components.preview-img-form')
 @endsection
