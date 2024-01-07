00d6df7026343dd51476b55a9491c5be5d94f086
rama-contoh <145236174+rama-contoh@users.noreply.github.com>
2024-01-07 11:05:06 +0700

    Dedesani melakukan Update index.blade.php

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -14,19 +14,19 @@
             @endif
             <div class="d-flex flex-wrap align-items-center justify-content-between mb-4">
                 <div>
-                    <h4 class="mb-3">Advance Salary List</h4>
-                    <p class="mb-0">A advance salary dashboard lets you easily gather and visualize advance salary data from optimizing <br>
-                        the advance salary experience, ensuring advance salary retention. </p>
+                    <h4 class="mb-3">Supplier List</h4>
+                    <p class="mb-0">A supplier dashboard lets you easily gather and visualize supplier data from optimizing <br>
+                        the supplier experience, ensuring supplier retention. </p>
                 </div>
                 <div>
-                <a href="{{ route('advance-salary.create') }}" class="btn btn-primary add-list"><i class="fas fa-plus mr-3"></i></i>Create Advance Salary</a>
-                <a href="{{ route('advance-salary.index') }}" class="btn btn-danger add-list"><i class="fa-solid fa-trash mr-3"></i>Clear Search</a>
+                    <a href="{{ route('suppliers.create') }}" class="btn btn-primary add-list"><i class="fa-solid fa-plus mr-3"></i>Add Supplier</a>
+                    <a href="{{ route('suppliers.index') }}" class="btn btn-danger add-list"><i class="fa-solid fa-trash mr-3"></i>Clear Search</a>
                 </div>
             </div>
         </div>
 
         <div class="col-lg-12">
-            <form action="{{ route('advance-salary.index') }}" method="get">
+            <form action="{{ route('suppliers.index') }}" method="get">
                 <div class="d-flex flex-wrap align-items-center justify-content-between">
                     <div class="form-group row">
                         <label for="row" class="col-sm-3 align-self-center">Row:</label>
@@ -44,7 +44,7 @@
                         <label class="control-label col-sm-3 align-self-center" for="search">Search:</label>
                         <div class="col-sm-8">
                             <div class="input-group">
-                                <input type="text" id="search" class="form-control" name="search" placeholder="Search employee" value="{{ request('search') }}">
+                                <input type="text" id="search" class="form-control" name="search" placeholder="Search supplier" value="{{ request('search') }}">
                                 <div class="input-group-append">
                                     <button type="submit" class="input-group-text bg-primary"><i class="fa-solid fa-magnifying-glass font-size-20"></i></button>
                                 </div>
@@ -62,50 +62,47 @@
                         <tr class="ligth ligth-data">
                             <th>No.</th>
                             <th>Photo</th>
-                            <th>@sortablelink('employee.name', 'name')</th>
-                            <th>@sortablelink('date')</th>
-                            <th>@sortablelink('advance_salary', 'advance salary')</th>
+                            <th>@sortablelink('name')</th>
+                            <th>@sortablelink('email')</th>
+                            <th>@sortablelink('phone')</th>
+                            <th>@sortablelink('shopname')</th>
+                            <th>@sortablelink('type')</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody class="ligth-body">
-                        @forelse ($advance_salaries as $advance_salary)
+                        @foreach ($suppliers as $supplier)
                         <tr>
-                            <td>{{ (($advance_salaries->currentPage() * 10) - 10) + $loop->iteration  }}</td>
+                            <td>{{ (($suppliers->currentPage() * 10) - 10) + $loop->iteration  }}</td>
                             <td>
-                                <img class="avatar-60 rounded" src="{{ $advance_salary->employee->photo ? asset('storage/employees/'.$advance_salary->employee->photo) : asset('assets/images/user/1.png') }}">
+                                <img class="avatar-60 rounded" src="{{ $supplier->photo ? asset('storage/suppliers/'.$supplier->photo) : asset('assets/images/user/1.png') }}">
                             </td>
-                            <td>{{ $advance_salary->employee->name }}</td>
-                            <td>{{ Carbon\Carbon::parse($advance_salary->date)->format('M/Y') }}</td>
-                            <td>{{ $advance_salary->advance_salary ? '$'.$advance_salary->advance_salary : 'No Advance' }}</td>
+                            <td>{{ $supplier->name }}</td>
+                            <td>{{ $supplier->email }}</td>
+                            <td>{{ $supplier->phone }}</td>
+                            <td>{{ $supplier->shopname }}</td>
+                            <td>{{ $supplier->type }}</td>
                             <td>
-                                <form action="{{ route('advance-salary.destroy', $advance_salary->id) }}" method="POST" style="margin-bottom: 5px">
-                                    @method('delete')
-                                    @csrf
-                                    <div class="d-flex align-items-center list-action">
-                                        <a class="btn btn-success mr-2" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"
-                                            href="{{ route('advance-salary.edit', $advance_salary->id) }}""><i class="ri-pencil-line mr-0"></i>
-                                        </a>
-                                        <button type="submit" class="btn btn-warning mr-2 border-none" onclick="return confirm('Are you sure you want to delete this record?')" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete">
-                                            <i class="ri-delete-bin-line mr-0"></i>
-                                        </button>
-                                    </div>
-                                </form>
+                                <div class="d-flex align-items-center list-action">
+                                    <a class="badge badge-info mr-2" data-toggle="tooltip" data-placement="top" title="" data-original-title="View"
+                                        href="{{ route('suppliers.show', $supplier->id) }}"><i class="ri-eye-line mr-0"></i>
+                                    </a>
+                                    <a class="badge bg-success mr-2" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"
+                                        href="{{ route('suppliers.edit', $supplier->id) }}""><i class="ri-pencil-line mr-0"></i>
+                                    </a>
+                                    <form action="{{ route('suppliers.destroy', $supplier->id) }}" method="POST" style="margin-bottom: 5px">
+                                        @method('delete')
+                                        @csrf
+                                        <button type="submit" class="badge bg-warning mr-2 border-none" onclick="return confirm('Are you sure you want to delete this record?')" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="ri-delete-bin-line mr-0"></i></button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
-
-                        @empty
-                        <div class="alert text-white bg-danger" role="alert">
-                            <div class="iq-alert-text">Data not Found.</div>
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <i class="ri-close-line"></i>
-                            </button>
-                        </div>
-                        @endforelse
+                        @endforeach
                     </tbody>
                 </table>
             </div>
-            {{ $advance_salaries->links() }}
+            {{ $suppliers->links() }}
         </div>
     </div>
     <!-- Page end  -->
